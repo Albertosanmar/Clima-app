dbb25021dd07c5a6c3c71cb79c599f62fc81b8c9
Alberto Sanmartin Taylor <albertosanmartintaylor@MacBook-Pro-de-Alberto.local>
2025-04-14 16:26:41 +0200

    modificación

--- src/componets/Form.js
+++ src/componets/Form.js
@@ -14,8 +14,8 @@ const Form = ({newLocation}) => {
     return(
         <div className="container">
             <form onSubmit={onSubmit}>
-                <div className="input-group mb-3 mx-auto">
-                    <input type="text" className="form-control" placeholder="Ciudad" onChange={(e) =>setCity(e.target.value)}/>
+                <div className="input-group mb-3 mt-5 pt-5 mx-auto">
+                    <input type="text" className="form-control" placeholder="Ciudad o pais" onChange={(e) =>setCity(e.target.value)}/>
                     <button className="btn btn-primary input-group-text" type="submit">Buscar</button>
                 </div>
             </form>
--- src/componets/NavBar.js
+++ src/componets/NavBar.js
@@ -1,11 +1,10 @@
 const NavBar = () => {
-
+//revisar logo <img className="img" src="public/SkyCast.png" alt="Logo"></img>
     return(
         <nav className="navbar-brand">
             <div className="container-fluid"> 
                 <h1 className="navbar text-light">SkyCast</h1>
-                <img src="./public/SkyCast.png" alt="Logo"></img>
-               
+                
             </div>
         </nav>
 
