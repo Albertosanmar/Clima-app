6d629133b72a091e1f51816f9c0687beb96bc846
Alberto Sanmartin Taylor <albertosanmartintaylor@MacBook-Pro-de-Alberto.local>
2025-04-14 13:52:17 +0200

    Favicon y modificación

--- src/componets/NavBar.js
+++ src/componets/NavBar.js
@@ -1,10 +1,12 @@
 const NavBar = () => {
-
+//revisar img
     return(
         <nav className="navbar">
-            <div className="navbar-brand text-light" href="#">
-                <h1 className="logo">SkyCast</h1>
-                
+            <div className="container-fluid"> 
+                <a className="navbar-brand text-light" href="#">
+                    <img src="/public/SkyCast.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"></img>
+                    SkyCast
+                </a>
             </div>
         </nav>
 
