704004e7d4ccf595582e1be938d12ff2bc51a7fe
Alberto Sanmartin Taylor <albertosanmartintaylor@MacBook-Pro-de-Alberto.local>
2025-04-16 11:11:23 +0200

    ajustes css

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,58 +2,53 @@ import React from 'react';
 import Spinner from './Spinner';
 
 const Card = ({loadingData, showData, weather, forecast}) => {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = `${day}/${month}/${year}`;
 
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    var date = day + '/' + month+ '/' + year;
+    let url = "";
+    let iconUrl = "";
+    let iconUrl3 = "";
+    let iconUrl6 = "";
+    let iconUrl9 = "";
+    let forecastDate3 = "";
+    let forecastDate6 = "";
+    let forecastDate9 = "";
 
-    var url = "";
-    var iconUrl = "";
-
-    var iconUrl3 = "";
-    var iconUrl6 = "";
-    var iconUrl9 = "";
-
-    var forecastDate3 = "";
-    var forecastDate6 = "";
-    var forecastDate9 = "";
-
-    
-
-    if (loadingData){
+    if (loadingData) {
         return <Spinner />;
     }
 
-    if(showData){
+    if (showData) {
         url = "https://openweathermap.org/img/w/";
         iconUrl = url + weather.weather[0].icon + ".png";
-
         iconUrl3 = url + forecast.list[1].weather[0].icon + ".png";
         iconUrl6 = url + forecast.list[2].weather[0].icon + ".png";
         iconUrl9 = url + forecast.list[3].weather[0].icon + ".png";
-
         forecastDate3 = forecast.list[1].dt_txt.substring(11, 13);
         forecastDate6 = forecast.list[2].dt_txt.substring(11, 13);
         forecastDate9 = forecast.list[3].dt_txt.substring(11, 13);
     }
-    
 
     return (
         <div className="mt-5">
             {
                 showData === true ? (
                     <div className="container text-center">  
-                        <div className="card border-dark bg-danger bg-opacity-25 text-light mb-3">
+                        <div className="card card-custom mb-3">
                             <div className="card-header">
-                                <h3 className="text">Condiciones Actuales</h3>
+                                <h3 className="card-title">Condiciones Actuales</h3>
                             </div>
-                            <div className="row order-last">
-                                <div className="col mt-5">
+                            <div className="row">
+                                <div className="col mt-3">
                                     <p className="card-text">{date}</p>
-                                    <h1 className="card-text">{weather.name}</h1>
-                                    <h1 className="card-text"><img src={iconUrl} alt="icon"/>{(weather.main.temp - 273.15).toFixed(1)}ºC</h1>
+                                    <h1 className="card-text fs-1">{weather.name}</h1>
+                                    <h1 className="card-text fs-1">
+                                        <img src={iconUrl} alt="icon" className="card-icon"/>
+                                        {(weather.main.temp - 273.15).toFixed(1)}ºC
+                                    </h1>
                                 </div>
                                 <div className="col py-3">
                                     <p className="card-text">Max: {(weather.main.temp_max - 273.15).toFixed(1)}ºC</p>
@@ -64,26 +59,32 @@ const Card = ({loadingData, showData, weather, forecast}) => {
                                 </div>
                             </div>    
                         </div>
-                        <div className="card mb-3 mx-auto border-dark bg-danger bg-opacity-25 text-light">
+                        <div className="card card-custom mb-3 mx-auto">
                             <div className="row g-0">
                                 <div className="card-header">
-                                    <h3 className="text">Previsión</h3>
+                                    <h3 className="card-title">Previsión</h3>
                                 </div>
-                                <div className="col order-last">
+                                <div className="col">
                                     <div className="row mt-4">
-                                        <div className="col">
+                                        <div className="col fs-3">
                                             <p>{forecastDate3}h</p>
-                                            <p className="description"><img src={iconUrl3} alt="icon"/></p>
+                                            <p className="description">
+                                                <img src={iconUrl3} alt="icon" />
+                                            </p>
                                             <p className="temp">{(forecast.list[1].main.temp - 273.15).toFixed(1)}ºC</p>
                                         </div>
-                                        <div className="col">
+                                        <div className="col fs-3">
                                             <p>{forecastDate6}h</p>
-                                            <p className="description"><img src={iconUrl6} alt="icon"/></p>
+                                            <p className="description">
+                                                <img src={iconUrl6} alt="icon" />
+                                            </p>
                                             <p className="temp">{(forecast.list[2].main.temp - 273.15).toFixed(1)}ºC</p>
                                         </div>
-                                        <div className="col">
+                                        <div className="col fs-3">
                                             <p>{forecastDate9}h</p>
-                                            <p className="description"><img src={iconUrl9} alt="icon"/></p>
+                                            <p className="description">
+                                                <img src={iconUrl9} alt="icon" />
+                                            </p>
                                             <p className="temp">{(forecast.list[3].main.temp - 273.15).toFixed(1)}ºC</p>
                                         </div>
                                     </div>
@@ -91,14 +92,11 @@ const Card = ({loadingData, showData, weather, forecast}) => {
                             </div>
                         </div>
                     </div>
-
-                ):(
+                ) : (
                     <h2 className="text-light">Tu radar meteorológico.</h2>
                 )
             }
-        
         </div>
-        
     );
 }
 
