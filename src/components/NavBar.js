396aa2b3633068e81803737b2580305459526099
Alberto Sanmartin Taylor <albertosanmartintaylor@MacBook-Pro-de-Alberto.local>
2025-04-17 14:37:45 +0200

    estilos actualizados

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,17 +17,10 @@ const NavBar = () => {
         <>
             <nav className="navbar-custom position-relative">
                 <div className="container-fluid d-flex justify-content-between align-items-center position-relative">
-                    <img 
-                        className="logo-img" 
-                        src="/logo-skycast.svg" 
-                        alt="Logo"
-                    />
+                    <img className="logo-img" src="/logo-skycast.svg" alt="Logo"/>
                     <h1 className="app-title">SkyCast</h1>
 
-                    <button 
-                        onClick={toggleModal}
-                        className="hamburger-btn"
-                    >
+                    <button onClick={toggleModal} className="hamburger-btn">
                         <span className="hamburger-line"></span>
                         <span className="hamburger-line"></span>
                         <span className="hamburger-line"></span>
@@ -37,44 +30,23 @@ const NavBar = () => {
 
             {isOpen && (
                 <div className="modal-overlay">
-                    <button 
-                        onClick={toggleModal}
-                        className="close-btn"
-                        aria-label="Cerrar"
-                    >
-                        <i className="fa fa-times" aria-hidden="true"></i>
-                    </button>
-
-
+                    <button onClick={toggleModal} className="close-btn" aria-label="Cerrar"><i className="fa fa-times" aria-hidden="true"></i></button>
                     <div className="mt-5 me-auto ms-4">
                         {!showAbout ? (
                             <ul className="list-unstyled">
                                 <li className="mb-3">
-                                    <button onClick={toggleAbout} className="btn btn-link text-light text-decoration-none p-0 about-button">
-                                        Acerca de SkyCast
-                                    </button>
+                                    <button onClick={toggleAbout} className="btn btn-link text-light text-decoration-none p-0 about-button">Acerca de SkyCast</button>
                                 </li>
                                 <li className="mb-3">
-                                    <a 
-                                        href="https://github.com/Albertosanmar/Clima-app" 
-                                        target="_blank" 
-                                        rel="noopener noreferrer" 
-                                        className="text-light text-decoration-none d-flex align-items-center gap-2 github-link"
-                                    >
-                                        <i className="fa fa-github me-2 github-icon"></i> GitHub
-                                    </a>
+                                    <a href="https://github.com/Albertosanmar/Clima-app" target="_blank" rel="noopener noreferrer" className="text-light text-decoration-none d-flex align-items-center gap-2 github-link"><i className="fa fa-github me-2 github-icon"></i> GitHub</a>
                                 </li>
                             </ul>
                         ) : (
                             <div>
-                                <button onClick={toggleAbout} className="btn btn-link text-light text-decoration-none p-0 mb-3">
-                                    ← Volver al menú
-                                </button>
+                                <button onClick={toggleAbout} className="btn btn-link text-light text-decoration-none p-0 mb-3">← Volver al menú</button>
                                 <div className="about-content">
                                     <h5 className='fs-2'>Acerca de SkyCast</h5>
-                                    <p>
-                                        SkyCast es tu AppWeb de confianza para conocer el clima en cualquier parte del mundo utilizando la API de OpenWeather. Esta aplicación está desarrollada con React, un framework de JavaScript y la libreria Bootstrap para los estilos en el editor de código VS Code. Es el proyecto final de grado en Desarrollo de Aplicaciones Multiplataforma de Alberto Sanmartín Taylor.
-                                    </p>
+                                    <p>SkyCast es tu AppWeb de confianza para conocer el clima en cualquier parte del mundo utilizando la API de OpenWeather. Esta aplicación está desarrollada con React, un framework de JavaScript y la libreria Bootstrap para los estilos en el editor de código VS Code. Es el proyecto final de grado en Desarrollo de Aplicaciones Multiplataforma de Alberto Sanmartín Taylor.</p>
                                 </div>
                             </div>
                         )}
