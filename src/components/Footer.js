8591dcacc363898c0762e350b1639080752c634c
Alberto Sanmartin Taylor <albertosanmartintaylor@MacBook-Pro-de-Alberto.local>
2025-06-03 18:46:59 +0200

    .env

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="text-light py-3 mt-auto">
             <div className="container d-flex justify-content-center align-items-center">
                 <img src="/logo-skycast.svg" alt="SkyCast Logo" className="footer-logo" />
-                <p className="ms-3 mb-0">© 2025 SkyCast</p>
+                <p className="font-monospace fs-4 ms-3 mb-0">© 2025 SkyCast</p>
             </div>
         </footer>
     );
